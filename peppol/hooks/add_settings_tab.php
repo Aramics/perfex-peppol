7cd79c3c6328134693831bec593b9793cc90427f
Madandola Hammed <aramicrown@gmail.com>
2025-11-18 11:22:01 +0100

    fix settings

--- peppol/hooks/add_settings_tab.php
+++ peppol/hooks/add_settings_tab.php
@@ -23,29 +23,3 @@ hooks()->add_action('admin_init', function () {
         $CI->app->add_settings_section_child('finance', PEPPOL_MODULE_NAME, $settings_tab);
     }
 });
-
-/**
- * Handle PEPPOL settings form submission
- */
-hooks()->add_action('settings_group_saved', function ($group) {
-    if ($group !== 'finance') {
-        return;
-    }
-
-    // Check if PEPPOL settings were submitted
-    $CI = &get_instance();
-    $peppol_settings = $CI->input->post('settings');
-
-    if (!$peppol_settings || !isset($peppol_settings['peppol_company_identifier'])) {
-        return;
-    }
-
-    $CI->load->model('peppol/peppol_model');
-
-    // Log settings update
-    $CI->peppol_model->log_activity([
-        'type' => 'settings_updated',
-        'message' => 'PEPPOL settings updated',
-        'staff_id' => get_staff_user_id()
-    ]);
-});
--- peppol/views/admin/settings.php
+++ peppol/views/admin/settings.php
@@ -1,16 +1,22 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-<!-- Nav tabs -->
-<ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active">
-        <a href="#peppol_general" aria-controls="peppol_general" role="tab" data-toggle="tab">
-            <?php echo _l('peppol_general_settings'); ?>
-        </a>
-    </li>
-</ul>
+<div class="horizontal-scrollable-tabs panel-full-width-tabs">
+    <div class="scroller arrow-left tw-mt-px"><i class="fa fa-angle-left"></i></div>
+    <div class="scroller arrow-right tw-mt-px"><i class="fa fa-angle-right"></i></div>
+    <!-- Nav tabs -->
+    <div class="horizontal-tabs">
+        <ul class="nav nav-tabs nav-tabs-horizontal" role="tablist">
+            <li role="presentation" class="active">
+                <a href="#peppol_general" aria-controls="peppol_general" role="tab" data-toggle="tab">
+                    <?php echo _l('peppol_general_settings'); ?>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
 
 <!-- Tab panes -->
-<div class="tab-content">
+<div class="tab-content mtop30">
     <!-- General Settings Tab -->
     <div role="tabpanel" class="tab-pane active" id="peppol_general">
         <?php
